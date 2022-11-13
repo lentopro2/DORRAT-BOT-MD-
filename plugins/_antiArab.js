42502dc936689b6957f906b70b7337479fa3ec44
lentopro2 <117599632+lentopro2@users.noreply.github.com>
2022-11-13 19:29:37 -0400

    Update _antiArab.js

--- plugins/_antiArab.js
+++ plugins/_antiArab.js
@@ -8,13 +8,123 @@ if (isBotAdmin && chat.antiArab) {
 		
 if (m.sender.startsWith('92' || '92')) {
 global.db.data.users[m.sender].banned = true
-m.reply(`💥 Anti árabes está activo para evitar spam\n\nHasta la próxima`)
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
    
 if (m.sender.startsWith('212' || '212')) {
 global.db.data.users[m.sender].banned = true
-m.reply(`💥 Anti árabes está activo para evitar spam\n\nHasta la próxima`)
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-   
+
+if (m.sender.startsWith('234' || '234')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 	
+
+if (m.sender.startsWith('229' || '229')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 		
+
+if (m.sender.startsWith('7' || '7')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('33' || '33')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('20' || '20')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 	
+
+if (m.sender.startsWith('240' || '240')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('354' || '354')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('994' || '994')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 	
+
+if (m.sender.startsWith('972' || '972')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('976' || '976')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('44' || '44')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('94' || '94')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('66' || '66')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
+
+if (m.sender.startsWith('62' || '62')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('90' || '90')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('91' || '91')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('420' || '420')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('223' || '223')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('48' || '48')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('49' || '49')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
+if (m.sender.startsWith('31' || '31')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+	
+if (m.sender.startsWith('43' || '43')) {
+global.db.data.users[m.sender].banned = true
+m.reply(`💥 Anti fakes está activo para evitar spam\n\nHasta la próxima`)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
+
 }}
 export default handler
